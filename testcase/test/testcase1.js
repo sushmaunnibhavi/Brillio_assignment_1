803848311fcfcbc63fba4aebf07db3b5f17bb61e
sushmaunnibhavi <sushmaunnibhavi@gmail.com>
2021-08-21 20:20:11 +0530

    Added files

---
+++ testcase/test/testcase1.js
@@ -0,0 +1,21 @@
+const assert=require('chai').assert;
+const app = require('../app');
+const sayHello=require('../app');
+
+describe('App',function(){
+
+it('app should return hello',function(){
+assert.equal(app(),'hello');
+
+});
+
+
+
+it('sayHello should return type string', function(){
+let result = sayHello();
+assert.typeOf(result,'string');
+});
+
+
+
+});
